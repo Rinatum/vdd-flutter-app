a67b2d83a38e15bea9ffc768e166b8b54c10b2a0
Rinat <tatarinatum@gmail.com>
2022-12-09 20:31:38 +0300

    fix flutter format

--- lib/data/theme_change.dart
+++ lib/data/theme_change.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class DarkThemePreference {
   static const themeStatus = "THEMESTATUS";
 
